bffdf0d80c4a141a7221e978797569bbca99de7b
danfern3 <danfern3@student.42madrid.com>
2025-10-30 16:49:48 +0100

    feat: render images

---
+++ src/solong/textures.c
@@ -0,0 +1,49 @@
+
+
+#include "../../inc/headers/so_long.h"
+
+
+#include <unistd.h>
+#ifndef WIDTH
+#define WIDTH 512
+#endif
+#ifndef HEIGHT
+#define HEIGHT 512
+#endif
+
+static void error(void)
+{
+	sleep(4);
+	puts(mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
+
+int32_t	main(void)
+{
+	// Start mlx
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "Test", true);
+	if (!mlx)
+        error();
+
+	// Try to load the file
+	mlx_texture_t* texture = mlx_load_png("./src/solong/temp.png");
+	if (!texture)
+        error();
+	
+	// Convert texture to a displayable image
+	mlx_image_t* img = mlx_texture_to_image(mlx, texture);
+	if (!img)
+        error();
+
+	// Display the image
+	if (mlx_image_to_window(mlx, img, 0, 0) < 0)
+        error();
+
+	mlx_loop(mlx);
+
+	// Optional, terminate will clean up any leftovers, this is just to demonstrate.
+	mlx_delete_image(mlx, img);
+	mlx_delete_texture(texture);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}

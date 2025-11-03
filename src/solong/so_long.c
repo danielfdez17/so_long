09976e9ce06e4ec1f6c03564f0a8f4db0467d928
danfern3 <danfern3@student.42madrid.com>
2025-11-03 08:26:32 +0100

    fix: error with file extension

--- src/solong/so_long.c
+++ src/solong/so_long.c
@@ -6,43 +6,38 @@
 /*   By: danfern3 <danfern3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/26 13:27:10 by danfern3          #+#    #+#             */
-/*   Updated: 2025/10/30 16:04:24 by danfern3         ###   ########.fr       */
+/*   Updated: 2025/11/03 08:25:54 by danfern3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/headers/so_long.h"
 
-// int main(int ac, char **av)
-// {
-// 	(void)ac;
-// 	char	*file_ext = NULL;
-// 	if (!av || !av[1])
-// 	{
-// 		ft_putendl_fd(MISSING_FILE_MSG, 2);
-// 		return (0);
-// 	}
-// 	file_ext = ft_strrchr(av[1], '.');
-// 	if (!file_ext || ft_strncmp(file_ext, ".ber", 4) != 0)
-// 	{
-// 		ft_putendl_fd(FILE_EXTENSION_MSG, 2);
-// 		return (0);
-// 	}
-// 	// ft_putendl_fd("Archivo correcto", 1);
-// 	read_map(av[1]);
-// 	return (0);
-// }
+static t_bool	check_file_extension(char **av)
+{
+	char	*file_ext;
 
-// static void	ft_error(void)
-// {
-// 	fprintf(stderr, "%s", mlx_strerror(mlx_errno));
-// 	exit(EXIT_FAILURE);
-// }
+	if (!av || !av[1])
+	{
+		ft_putendl_fd(MISSING_FILE_MSG, 2);
+		return (false);
+	}
+	file_ext = ft_strrchr(av[1], '.');
+	if (!file_ext || ft_strncmp(file_ext, ".ber", 5) != 0)
+	{
+		ft_putendl_fd(FILE_EXTENSION_MSG, 2);
+		return (false);
+	}
+	return (true);
+}
 
-// static void	ft_hook(void *param)
-// {
-// 	const	mlx_t* mlx = param;
-// 	printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
-// }
+int main(int ac, char **av)
+{
+	(void)ac;
+	if (!check_file_extension(av))
+		return (0);
+	read_map(av[1]);
+	return (0);
+}
 
 // ! DEBUGGING/TESTING MAIN
 // int	main(void)
